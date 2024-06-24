3da8884d0a6ca535a7fec235da356e58867c6ae8
elijahizkiel <145738485+elijahizkiel@users.noreply.github.com>
2024-06-24 22:19:15 +0300

    constructor method and close method created for Server class

--- networking project/ChatApp/src/Server.java
+++ networking project/ChatApp/src/Server.java
@@ -1,5 +1,50 @@
+package net.
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private int port = 0;// initializing port for the server
+    private ServerSocket serverSocket = null;//serverSocket
+    private Socket socket = null;
+    private DataInputStream input = null;
+
+    public Server(int port){
+        //sets  for the server and initiate Socket from the server socket and input streams to read information from
+        // the network created
+        this.port = port;
+        try{
+            serverSocket = new ServerSocket(this.port);//creating serverside socket with specific socket
+
+            System.out.println("Server side port created \nwaiting to receive client request...");
+
+            socket = serverSocket.accept(); // accepting client request and forming link
+            System.out.println("client request accepted");
+
+            input = new DataInputStream(new BufferedInputStream(System.in));
+
+        }catch(IOException ioE){
+            ioE.printStackTrace();
+        }
+    }
+
+
+
+    public void closeConnection() {
+
+        //to close every connection that has been made and every I/O stream opened
+
+        try {
+            serverSocket.close();
+            socket.close();
+            input.close();
+        } catch (IOException ioE) {
+            System.out.println(ioE.getMessage());
+        }
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new Server(12345);
     }
 }
