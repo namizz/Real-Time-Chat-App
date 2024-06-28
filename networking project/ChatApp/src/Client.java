313f18311d09f117c895176ccd5bf89a9cd31621
Naomi Shiferaw <naomizerfu@gmail.com>
2024-06-28 14:42:06 -0700

    change

--- networking project/ChatApp/src/Client.java
+++ networking project/ChatApp/src/Client.java
@@ -1,2 +1,32 @@
+import java.net.*;
+import javax.swing.JOptionPane;
+import java.io.*;
+
 public class Client {
+    PrintWriter writer;
+    BufferedReader reader;
+    Socket sock;
+    public static void main(String[] args) {
+        new Client().ClientCode();
+        
+    }
+    public void ClientCode(){
+        try {
+            sock = new Socket("127.0.0.1", 4518);//connects with the server
+            writer = new PrintWriter(sock.getOutputStream());//write on the OutputStraem of the Socket
+            writer.println("hi server");//writing on the socket outputstream
+            writer.flush();
+            writer.println("Paramparas");
+            writer.flush();
+            //not yet important
+            InputStreamReader read = new InputStreamReader(sock.getInputStream());//reads the message on the socket
+            reader = new BufferedReader(read);//buffer the message
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Failed to connect");
+        
+        } 
+    }
+
 }
--- networking project/ChatApp/src/GUI.java
+++ networking project/ChatApp/src/GUI.java
@@ -1,3 +1,71 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
 public class GUI {
+    JFrame frame;
+    JTextArea chat;
+    JTextField sending;
+    JPanel panel;
+    JButton send;
+    Client message = new Client();
     
+    public static void main(String[] args) {
+        new GUI().chatbox();
+        
+    }
+    public void chatbox() {
+        message.ClientCode();
+    
+        frame = new JFrame("ChatApp");
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+    
+        chat = new JTextArea(15, 30);
+        chat.setEditable(false);
+        JScrollPane chatScrollPane = new JScrollPane(chat);
+        chatScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        chatScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    
+        sending = new JTextField(10);
+        send = new JButton("Send");
+        send.addActionListener(new ButtonListener());
+        Thread t = new Thread(new Chat());
+        t.start();
+    
+        panel.add(chatScrollPane, BorderLayout.CENTER);
+        panel.add(sending, BorderLayout.SOUTH);
+        panel.add(send, BorderLayout.EAST);
+    
+        frame.add(panel);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+    }
+    
+    public class ButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            message.writer.println(sending.getText());
+            sending.setText("");
+                        
+            }
+    
+    }
+    public class Chat implements Runnable{
+        public void run(){
+            String line;
+    
+            try{
+            while ((line = message.reader.readLine())!= null) {
+                System.out.println(line);
+                chat.append(line+"\n");
+            }
+        }
+            catch(IOException ex){
+                ex.printStackTrace();
+
+            }            
+        }
+    }
 }
